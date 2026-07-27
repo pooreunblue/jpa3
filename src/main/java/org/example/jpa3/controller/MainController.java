08eb241618b19634191629bc7072caefd14874b4
bsh <mjbsh02@mju.ac.kr>
2026-07-27 10:24:08 +0900

    feat: 전화번호 등록 및 목록 화면 연결

--- src/main/java/org/example/jpa3/controller/MainController.java
+++ src/main/java/org/example/jpa3/controller/MainController.java
@@ -1,14 +1,30 @@
 package org.example.jpa3.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.jpa3.dto.PhoneFormDTO;
+import org.example.jpa3.service.PhoneService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
+@RequiredArgsConstructor
 public class MainController {
+    private final PhoneService phoneService;
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("phones", phoneService.findAll());
         return "index";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute PhoneFormDTO dto) {
+        phoneService.save(dto.toEntity());
+        return "redirect:/";
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,16 +1,21 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: baegseungho
-  Date: 2026. 7. 27.
-  Time: 오전 9:33
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
 <html>
 <head>
     <title>JPA 심화</title>
 </head>
 <body>
-    <h1>JPA 심화</h1>
+<h1>JPA 심화</h1>
+<section>
+    <c:forEach items="${phones}" var="phone">
+        <p>${phone}</p>
+    </c:forEach>
+</section>
+<section>
+    <form method="post">
+        <input name="name" placeholder="폰 이름 입력">
+        <button>생성</button>
+    </form>
+</section>
 </body>
 </html>
