fcd334f8e3d00b9e744528ade2cdfe1fa1e9fecd
bsh <mjbsh02@mju.ac.kr>
2026-07-28 17:12:28 +0900

    fix: Querydsl 프로세서와 감사 설정 보완

--- src/main/java/org/example/jpa3/config/JPAConfig.java
+++ src/main/java/org/example/jpa3/config/JPAConfig.java
@@ -5,10 +5,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing // BaseEntity -> AuditingEntityListener
 public class JPAConfig {
 
     @PersistenceContext
